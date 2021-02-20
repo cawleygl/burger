09c6cad32c3f4c6c53db27f16ed322dd69b32157
cawleygl <grantlcawley@gmail.com>
2021-02-20 14:49:59 -0600

    Added orm object with selectAll, insertOne, and updateOne functions

--- config/orm.js
+++ config/orm.js
@@ -1,15 +1,37 @@
 const connection = require('./connection.js');
 
-const selectAll = () => {
-// MYSQL
-};
-
-const insertOne = () => {
-// MYSQL
-};
-
-const updateOne = () => {
-// MYSQL
+const orm = {
+    selectAll(cb) {
+        const queryString = `SELECT * FROM burgers;`;
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    insertOne(burgerName, cb) {
+        let queryString = `INSERT INTO burgers (burger_name) VALUES (${burgerName});`;
+        console.log(queryString);
+        connection.query(queryString, vals, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne(burgerID, cb) {
+        let queryString = `UPDATE burgers SET devoured = !devoured WHERE id = ${burgerID};`;
+        console.log(queryString);
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
 };
 
 module.exports = orm;
