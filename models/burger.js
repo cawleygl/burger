8340708153f1af7bee1950d0383c342faf1f5771
cawleygl <grantlcawley@gmail.com>
2021-02-20 15:00:05 -0600

    Updated model to use simpler orm functions

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,15 @@
 const orm = require('../config/orm.js');
 
 const burger = {
+    selectAll() {
+        orm.selectAll((res) => cb(res));
+    },
+    insertOne(burgerName) {
+        orm.insertOne(burgerName, (res) => cb(res));
+    },
+    updateOne(burgerID) {
+        orm.insertOne(burgerID, (res) => cb(res));
+    },
 };
 
 module.exports = burger;
