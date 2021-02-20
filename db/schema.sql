38112352cc92a1d4d60a2e03ec5e79a2090bd936
cawleygl <grantlcawley@gmail.com>
2021-02-20 14:35:48 -0600

    Increased character count for burger-name

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers (
   id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id),
-  burger_name VARCHAR(30) NOT NULL,
+  burger_name VARCHAR(100) NOT NULL,
   devoured BOOLEAN NOT NULL DEFAULT 0
 );
