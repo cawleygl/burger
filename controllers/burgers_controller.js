5262aed3f43b09e6b94cc9be73585487bae72e1d
cawleygl <grantlcawley@gmail.com>
2021-02-28 22:51:50 -0500

    Added get route to list current burger names

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,17 @@
 const express = require('express');
-const cat = require('../models/burger.js');
+const burger = require('../models/burger.js');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    burger.selectAll((data) => {
+      const dataObj = {
+        burgers: data,
+      };
+      console.log(dataObj);
+      res.render('index', dataObj);
+    });
+  });
+  
 
 module.exports = router;
