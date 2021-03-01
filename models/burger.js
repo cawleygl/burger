bc169068c52bbf6d88cb8f7ef0e094dcb52e4f29
cawleygl <grantlcawley@gmail.com>
2021-02-28 22:53:17 -0500

    Reverted functions to include table input parameters

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,14 @@
 const orm = require('../config/orm.js');
 
 const burger = {
-    selectAll() {
-        orm.selectAll((res) => cb(res));
+    selectAll(cb) {
+        orm.selectAll('burgers', (res) => cb(res));
     },
     insertOne(burgerName) {
-        orm.insertOne(burgerName, (res) => cb(res));
+        orm.insertOne('burgers', burgerName, (res) => cb(res));
     },
     updateOne(burgerID) {
-        orm.insertOne(burgerID, (res) => cb(res));
+        orm.insertOne('burgers', burgerID, (res) => cb(res));
     },
 };
 
